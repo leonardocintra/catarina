eeb91ae5dd3504394276bf38130b8832d74dffa0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-18 13:53:58 -0300

    :lipstick: add login page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>{children}
+      RONAAAAAAAALDO
+      </body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,60 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
-  return <main>
-    <Button>Teste</Button>
-  </main>;
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">CNC - Gestão</h1>
+            <p className="text-balance text-muted-foreground">
+              Entre com seu email ou CPF para fazer login no sistema
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email ou CPF</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Senha</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Esqueceu sua senha ?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Link href={"/dashboard"}>
+              <Button type="submit" className="w-full">
+                Entrar
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/jesus.jpg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
 }
