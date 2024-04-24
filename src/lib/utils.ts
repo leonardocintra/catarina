0bfd7f6cee04615db86ee85c511cec33f678a9b2
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-24 12:27:59 -0300

    :bug: identificando bug deploy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,8 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const AmbrosioBaseUrl =
   process.env.AMBROSIO_URL || "http://localhost:3005";
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 
 // apagar se nao for usar swr
 // export const fetcher = (...args: Parameters<typeof fetch>) =>
