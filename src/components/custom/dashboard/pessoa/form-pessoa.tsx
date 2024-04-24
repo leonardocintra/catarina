25bff038800db1d5d45201423fcbd889cc5ebaa5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-24 13:36:59 -0300

    :alembic: gambi

---
+++ src/components/custom/dashboard/pessoa/form-pessoa.tsx
@@ -0,0 +1,223 @@
+"use client";
+
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { IEstadoCivil } from "@/interfaces/IEstadoCivil";
+import { IEscolaridade } from "@/interfaces/IEscolaridade";
+import { ITipoCarisma } from "@/interfaces/ITipoCarisma";
+import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
+
+type PessoaFormProps = {
+  urlBase: string;
+};
+
+export default function PessoaForm({ urlBase }: PessoaFormProps) {
+  const [estadoCivils, setEstadoCivils] = useState<IEstadoCivil[]>();
+  const [escolaridades, setEscolaridades] = useState<IEscolaridade[]>();
+  const [tipoCarismas, setTipoCarismas] = useState<ITipoCarisma[]>();
+
+  async function getEstadoCivil() {
+    const res = await fetch(
+      `${urlBase}/api/ambrosio/configuracoes/estadoCivil`
+    );
+    return res.json();
+  }
+
+  async function getEscolaridade() {
+    const res = await fetch(
+      `${urlBase}/api/ambrosio/configuracoes/escolaridade`
+    );
+    return res.json();
+  }
+
+  async function getTipoCarisma() {
+    const res = await fetch(
+      `${urlBase}/api/ambrosio/configuracoes/tipoCarisma`
+    );
+    return res.json();
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [resEstadoCivil, resEscolaridade, resTipoCarisma] =
+          await Promise.all([
+            getEstadoCivil(),
+            getEscolaridade(),
+            getTipoCarisma(),
+          ]);
+
+        setEstadoCivils(resEstadoCivil.data);
+        setEscolaridades(resEscolaridade.data);
+        setTipoCarismas(resTipoCarisma.data);
+      } catch (error: any) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const formSchema = z.object({
+    nome: z
+      .string({ message: "Campo obrigatório" })
+      .min(2, { message: "Nome deve ter no minimo 2 caracteres." })
+      .max(50),
+    nacionalidade: z.string().max(50),
+    estadoCivil: z.number().min(1),
+    escolaridade: z.number().min(1),
+    tipoCarima: z.number().min(1),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nome: "",
+      nacionalidade: "brasileira",
+    },
+  });
+
+  if (!estadoCivils || !escolaridades || !tipoCarismas) {
+    return (
+      <div>
+        <h2>Carregando ...</h2>
+      </div>
+    );
+  }
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="max-w-md mx-auto sm:mt-8">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="flex flex-col gap-3"
+        >
+          <FormField
+            control={form.control}
+            name="nome"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nome completo ..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="estadoCivil"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Estado Civil</FormLabel>
+                <Select onValueChange={field.onChange}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {estadoCivils.map((es) => (
+                      <SelectItem key={es.id} value={es.id.toString()}>
+                        {es.descricao}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="escolaridade"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Escolaridade</FormLabel>
+                <Select onValueChange={field.onChange}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {escolaridades.map((es) => (
+                      <SelectItem key={es.id} value={es.id.toString()}>
+                        {es.descricao}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="tipoCarima"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Carisma</FormLabel>
+                <Select onValueChange={field.onChange}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {tipoCarismas.map((es) => (
+                      <SelectItem key={es.id} value={es.id.toString()}>
+                        {es.descricao}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="nacionalidade"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nacionalidade</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nacionalidade ..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Salvar</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,11 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const AmbrosioBaseUrl =
-  process.env.AMBROSIO_URL ||
-  "https://caminho-neocatecumenal-ambrosio.vprjdh.easypanel.host";
-export const BASE_URL =
-  process.env.BASE_URL ||
-  "https://caminho-neocatecumenal-catarina.vprjdh.easypanel.host";
+  process.env.AMBROSIO_URL || "http://localhost:3005";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 
 // apagar se nao for usar swr
 // export const fetcher = (...args: Parameters<typeof fetch>) =>
