ffa9744c58c959a898fe17b9e0bc6b325c4c4ef7
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-24 13:10:09 -0300

    :bug: url to prod

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const AmbrosioBaseUrl =
-  process.env.AMBROSIO_URL || "http://localhost:3005";
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+  process.env.AMBROSIO_URL ||
+  "https://caminho-neocatecumenal-ambrosio.vprjdh.easypanel.host";
+export const BASE_URL =
+  process.env.BASE_URL ||
+  "https://caminho-neocatecumenal-catarina.vprjdh.easypanel.host";
 
 // apagar se nao for usar swr
 // export const fetcher = (...args: Parameters<typeof fetch>) =>
