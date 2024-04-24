7e25ef6f75561ad9cd18b94c5ff1923f55b91a16
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-24 12:32:50 -0300

    :bug: fix easypanel .env

--- src/app/dashboard/pessoas/page.tsx
+++ src/app/dashboard/pessoas/page.tsx
@@ -14,60 +14,55 @@ import { BASE_URL } from "@/lib/utils";
 import Link from "next/link";
 
 export default async function ComunidadePage() {
+  const getPessoas = async () => {
+    const res = await fetch(`${BASE_URL}/api/ambrosio/pessoa`);
+    return res.json();
+  };
+  const data = await getPessoas();
+
+  const pessoas: IPessoa[] = data.data;
+
+  // https://youtu.be/oGq9o2BxlaI?t=1400
+
   return (
-    <div>
-      <h2>Teste {BASE_URL}</h2>
+    <div className="">
+      <div>
+        <div className="flex justify-between">
+          <h2 className="text-3xl pl-3">Pessoas - {pessoas?.length}</h2>
+          <Link href={"/dashboard/pessoas/novo"}>
+            <Button>Cadastrar</Button>
+          </Link>
+        </div>
+        <Separator className="mb-3 mt-2" />
+      </div>
+
+      <Table>
+        <TableCaption>
+          Ultimos cadatros - Pessoas: {pessoas?.length}
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Nome</TableHead>
+            <TableHead>Carisma</TableHead>
+            <TableHead>Ação</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {pessoas?.map((pessoa) => (
+            <TableRow key={pessoa.id}>
+              <TableCell>#{pessoa.id}</TableCell>
+              <TableCell className="font-bold">{pessoa.nome}</TableCell>
+              <TableCell>{pessoa.tipoCarisma.descricao}</TableCell>
+              <TableCell>
+                <Button variant={"link"} size={"sm"}>
+                  Editar
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
-  // const getPessoas = async () => {
-  //   const res = await fetch(`${BASE_URL}/api/ambrosio/pessoa`);
-  //   return res.json();
-  // };
-  // const data = await getPessoas();
-
-  // const pessoas: IPessoa[] = data.data;
-
-  // // https://youtu.be/oGq9o2BxlaI?t=1400
-
-  // return (
-  //   <div className="">
-  //     <div>
-  //       <div className="flex justify-between">
-  //         <h2 className="text-3xl pl-3">Pessoas - {pessoas?.length}</h2>
-  //         <Link href={"/dashboard/pessoas/novo"}>
-  //           <Button>Cadastrar</Button>
-  //         </Link>
-  //       </div>
-  //       <Separator className="mb-3 mt-2" />
-  //     </div>
-
-  //     <Table>
-  //       <TableCaption>
-  //         Ultimos cadatros - Pessoas: {pessoas?.length}
-  //       </TableCaption>
-  //       <TableHeader>
-  //         <TableRow>
-  //           <TableHead>ID</TableHead>
-  //           <TableHead>Nome</TableHead>
-  //           <TableHead>Carisma</TableHead>
-  //           <TableHead>Ação</TableHead>
-  //         </TableRow>
-  //       </TableHeader>
-  //       <TableBody>
-  //         {pessoas?.map((pessoa) => (
-  //           <TableRow key={pessoa.id}>
-  //             <TableCell>#{pessoa.id}</TableCell>
-  //             <TableCell className="font-bold">{pessoa.nome}</TableCell>
-  //             <TableCell>{pessoa.tipoCarisma.descricao}</TableCell>
-  //             <TableCell>
-  //               <Button variant={"link"} size={"sm"}>
-  //                 Editar
-  //               </Button>
-  //             </TableCell>
-  //           </TableRow>
-  //         ))}
-  //       </TableBody>
-  //     </Table>
-  //   </div>
-  // );
 }
