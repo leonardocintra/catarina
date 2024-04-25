e783664cece26433baaa92db6c9902ccdf70bbc7
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-25 13:46:10 -0300

    :construction: add next_public_

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const AmbrosioBaseUrl =
-  process.env.AMBROSIO_URL || "http://localhost:3005";
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_AMBROSIO_URL || "http://localhost:3005";
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 // apagar se nao for usar swr
 // export const fetcher = (...args: Parameters<typeof fetch>) =>
