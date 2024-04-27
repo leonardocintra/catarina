96d16ada1582a66f20a075da280cf31e2a3443fb
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-27 16:51:56 -0300

    :bug: vai que

--- src/app/api/ambrosio/regioes/route.ts
+++ src/app/api/ambrosio/regioes/route.ts
@@ -3,9 +3,9 @@ import { NextRequest } from "next/server";
 
 const url = `${AmbrosioBaseUrl}/regioes`;
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const res = await fetch(url);
 
   const data = await res.json();
-  return Response.json(data);
+  return Response.json({ data });
 }
