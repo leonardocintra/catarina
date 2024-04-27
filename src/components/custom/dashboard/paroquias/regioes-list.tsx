4af9d47c8d998591f161f7192ba42a3a2090edf8
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-04-27 16:46:24 -0300

    :ambulance: por que nao vai

--- src/components/custom/dashboard/paroquias/regioes-list.tsx
+++ src/components/custom/dashboard/paroquias/regioes-list.tsx
@@ -32,7 +32,7 @@ export default function RegioesCaminho({ baseUrl }: RegioesCaminhoProps) {
   useEffect(() => {
     const fetchData = async () => {
       async function getRegioes() {
-        const res = await fetch(`${baseUrl}/api/ambrosio/configuracoes/estadoCivil`);
+        const res = await fetch(`${baseUrl}/api/ambrosio/regioes`);
         return res.json();
       }
 
