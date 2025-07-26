0c62a81d766abbff0cdb3c2180b117b6dcb2696b
Leonardo Cintra <leonardo.ncintra@outlook.com>
2025-07-26 15:00:09 -0300

    :lipstick: modificando chart

---
+++ src/components/custom/dashboard/relatorios/chart/chart-carismas.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { TrendingUp } from "lucide-react";
+import { LabelList, Pie, PieChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+export const description = "A pie chart with a label list";
+
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Leigos",
+    color: "var(--chart-1)",
+  },
+  safari: {
+    label: "Bispos",
+    color: "var(--chart-2)",
+  },
+  firefox: {
+    label: "Familia Missão",
+    color: "var(--chart-3)",
+  },
+  edge: {
+    label: "Vocacionados(as)",
+    color: "var(--chart-4)",
+  },
+  other: {
+    label: "Outros",
+    color: "var(--chart-5)",
+  },
+} satisfies ChartConfig;
+
+export function ChartCarisma() {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Carismas</CardTitle>
+        <CardDescription>Pessoas e carismas</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="[&_.recharts-text]:fill-background mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              content={<ChartTooltipContent nameKey="visitors" hideLabel />}
+            />
+            <Pie data={chartData} dataKey="visitors">
+              <LabelList
+                dataKey="browser"
+                className="fill-background"
+                stroke="none"
+                fontSize={12}
+                formatter={(value: keyof typeof chartConfig) =>
+                  chartConfig[value]?.label
+                }
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="text-muted-foreground leading-none">
+          Mostrando todos os carismas por pessoa
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
